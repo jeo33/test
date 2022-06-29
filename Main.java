9c271e5347fa95861cc046b6ff1b360317ce2523
junpeng gai <34943011+jeo33@users.noreply.github.com>
2022-06-28 21:38:34 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package project;
+
+import javax.swing.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        jf s= new jf();
+
+    }
+
+}
+
+
---
+++ jf.java
@@ -0,0 +1,19 @@
+package project;
+
+import javax.swing.*;
+
+public class jf extends JFrame {
+    demo panel=new demo();
+
+    jf()
+    {
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("tjos os tje tott;e");
+        this.setVisible(true);
+        this.add(panel);
+
+
+    }
+
+}
