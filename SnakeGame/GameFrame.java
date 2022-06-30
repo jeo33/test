84b084df2518c3f4a27c5aecae2cd6623d64df93
junpeng gai <34943011+jeo33@users.noreply.github.com>
2022-06-30 00:55:27 -0400

    Add files via upload

--- SnakeGame/GameFrame.java
+++ SnakeGame/GameFrame.java
@@ -1,25 +1,35 @@
 package snake;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.sql.Time;
 
-public class GameFrame extends JFrame  {
+public class GameFrame extends JFrame implements ChangeListener {
+
+    JPanel backG=new JPanel();
 
     GameFrame()
 
     {
+
         this.add(new GamePanel());
         this.setTitle("Snake Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
         this.setResizable(false);
         this.pack();
+        this.setVisible(true);
         this.setLocationRelativeTo(null);
 
-        //Start();
+    }
 
-    }}
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+
+    }
+}
--- SnakeGame/GamePanel.java
+++ SnakeGame/GamePanel.java
@@ -11,28 +11,42 @@ import java.sql.Time;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
-    static final int Width_Screen=600;
-    static final int Height_Screen=600;
-    static final int UNIT_SIZE=25;
+	int bodyType;
+	ImageIcon p1=new ImageIcon("p1.png");
+	ImageIcon p2=new ImageIcon("p2.png");
+	ImageIcon p3=new ImageIcon("p3.png");
+	ImageIcon[] p={p1,p2,p3};
+	ImageIcon Head_U=new ImageIcon("headU.png");
+	ImageIcon Head_D=new ImageIcon("headD.png");
+	ImageIcon Head_L=new ImageIcon("headL.png");
+	ImageIcon Head_R=new ImageIcon("headR.png");
+	ImageIcon stella=new ImageIcon("stella.png");
+	ImageIcon mama=new ImageIcon("mamaIcon.png");
+	ImageIcon baba=new ImageIcon("babaIcon.png");
+	ImageIcon background=new ImageIcon("BACKGROUND.jpg");
+
+    static final int Width_Screen=800;
+    static final int Height_Screen=800;
+    static final int UNIT_SIZE=50;
     static final int GAME_UNITS=(Width_Screen*Height_Screen)/UNIT_SIZE;
     final int x[]=new int[Width_Screen];
     final int y[]=new int[Height_Screen];
     int BodyUnitsStart=3;
     int AppleEaton;
+	int AppleType;
     int AppleX;
     int AppleY;
     int delay=300;
-    int tempx;
-    int tempy;
     char keyPressed;
     Timer timer;
     Random random;
     GamePanel()
     {
 
-        this.setPreferredSize(new Dimension(Width_Screen,Height_Screen));
-        this.setBackground(Color.black);
+        this.setPreferredSize(new Dimension(background.getIconWidth(),background.getIconHeight()));
         random=new Random();
+		this.setBackground(Color.blue);
+		this.setBounds(0,0,Width_Screen,Width_Screen);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdaptor());
         Start();
@@ -41,34 +55,27 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void Start()
     {
-    	for(int i=0;i<3;i++)
+    	for(int i=0;i<BodyUnitsStart;i++)
     	{
     		if(i==0)
     		{
 
-   			 x[i]=(random.nextInt((int)Width_Screen/UNIT_SIZE)-2*BodyUnitsStart+3)*UNIT_SIZE;
-   			 y[i]=(random.nextInt((int)Height_Screen/UNIT_SIZE)-2*BodyUnitsStart+3)*UNIT_SIZE;
+   			 x[i]=(random.nextInt((int)Width_Screen/UNIT_SIZE-4*BodyUnitsStart)+3)*UNIT_SIZE;
+   			 y[i]=(random.nextInt((int)Height_Screen/UNIT_SIZE-4*BodyUnitsStart)+3)*UNIT_SIZE;
+				System.out.println("x : "+x[i]+"\n"+"y : "+y[i]);
     		}
     		else
     		{
-    			x[i]=x[0]-i*25;
+    			x[i]=x[0]-i*UNIT_SIZE;
     			y[i]=y[0];
+				System.out.println("x : "+x[i]+"\n"+"y : "+y[i]);
     		}
 
     	}
         timer=new Timer(delay,this);
         timer.start();
-        
-		
-	       
-	        newApple();
-	        	
-	        
-	        repaint();
-		
-
-
-
+		newApple();
+		repaint();
     }
     public boolean Eaton()
     {
@@ -77,6 +84,7 @@ public class GamePanel extends JPanel implements ActionListener {
     		if((x[i]==AppleX)&&(y[i]==AppleY))
     		{
     			BodyUnitsStart++;
+				AppleEaton++;
     			newApple();
     			return true;
     		}
@@ -93,28 +101,72 @@ public class GamePanel extends JPanel implements ActionListener {
 
      public void draw(Graphics g)
     {
-    	 g.setColor(Color.CYAN);
-        
-        for(int i =0;i<Height_Screen/UNIT_SIZE;i++)
-        {
-            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,Height_Screen);
+		background.paintIcon(this,g,0,0);
 
-            g.drawLine(0,i*UNIT_SIZE,Width_Screen,i*UNIT_SIZE);
+		g.setColor(Color.BLACK);
+		if(collision())
+		{
+			g.setFont(new Font("Ink Free", Font.BOLD, 50));
+			g.drawString("Score: " + AppleEaton, 1000, 50);
+			g.setFont(new Font("Ink Free",Font.BOLD,80));
+			g.setColor(Color.RED);
+			g.drawString("You Lost",930,130);
+		}
+		else {
+			g.setFont(new Font("Ink Free", Font.BOLD, 50));
+			g.drawString("Score: " + AppleEaton, 1000, 50);
+		}
+		 /*
+		 g.drawLine(0,0, 0,Height_Screen);
+		 g.drawLine(Width_Screen,Height_Screen, Width_Screen,0);
+		 g.drawLine(0,0,Width_Screen,0);
+		 g.drawLine(Width_Screen,Height_Screen, 0,Height_Screen);
+		 g.drawLine(Width_Screen,Height_Screen, Width_Screen,0);
+		  */
+		g.setColor(Color.black);
+		for(int i=0;i<Height_Screen/UNIT_SIZE;i++)
+		{
+			g.drawLine(0,i*UNIT_SIZE, Width_Screen,i*UNIT_SIZE);
+			g.drawLine(i*UNIT_SIZE,Height_Screen, i*UNIT_SIZE,0);
+		}
 
-        }
+		g.drawLine(Width_Screen,Height_Screen, 0,Height_Screen);
+		g.drawLine(Width_Screen,Height_Screen, Width_Screen,0);
+
         g.setColor(Color.GREEN);
-        g.fillOval(AppleX,AppleY,UNIT_SIZE,UNIT_SIZE);
-        
+		switch (AppleType) {
+			case -1 -> {
+				mama.paintIcon(this, g, AppleX, AppleY);
+				delay -= 20;
+			}
+			case 0 -> stella.paintIcon(this, g, AppleX, AppleY);
+			case 1 -> {
+				baba.paintIcon(this, g, AppleX, AppleY);
+				delay += 20;
+			}
+		}
+
         g.setColor(Color.red);
    	 for(int i=0;i< BodyUnitsStart;i++)
    	 {
    		 if(i==0)
-   		 {
-   			 g.fillOval(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+   		 {if(!collision()) {
+
+			 switch (keyPressed) {
+				 case 'w' -> Head_U.paintIcon(this, g, x[0], y[0]);
+				 case 'a' -> Head_L.paintIcon(this, g, x[0], y[0]);
+				 case 's' -> Head_D.paintIcon(this, g, x[0], y[0]);
+				 case 'd' -> Head_R.paintIcon(this, g, x[0], y[0]);
+				 default -> keyPressed = 's';
+			 }
+		 }
+
+
    		 }
    		 else
    		 {
-   			 g.fillOval(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				bodyType=random.nextInt(3);
+   			p[bodyType].paintIcon(this,g,x[i],y[i]);
    		 }
    	 }
 }
@@ -122,15 +174,28 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public  void newApple()
     {
+		AppleType=random.nextInt(3)-1;
         AppleX=random.nextInt((int)Width_Screen/UNIT_SIZE)*UNIT_SIZE;
         AppleY=random.nextInt((int)Height_Screen/UNIT_SIZE)*UNIT_SIZE;
-
     }
     
-    
+    public boolean collision()
+	{
+		boolean crush=(x[0]<0||y[0]<0||x[0]==Width_Screen||y[0]==Width_Screen);
+		boolean loop=false;
+		for(int i=1;i<BodyUnitsStart;i++)
+		{
+			if(x[0]==x[i]&&y[0]==y[i])
+			{
+				loop=true;
+				break;
+			}
+		}
+		return loop||crush;
+
+	}
     public void move()
     {
-    	
 		switch(keyPressed)
 		{
 		case'w':
@@ -148,7 +213,7 @@ public class GamePanel extends JPanel implements ActionListener {
 		default:
 			keyPressed='s';
 			break;
-		
+
 			}
     }
     
@@ -158,10 +223,7 @@ public class GamePanel extends JPanel implements ActionListener {
     	{
     		if(i==0)
     		{
-    			
-
-   			 y[i]=y[i+1]-25;
-   			 
+   			 y[i]=y[i+1]-UNIT_SIZE;
     		}
     		else
     		{
@@ -179,10 +241,7 @@ public class GamePanel extends JPanel implements ActionListener {
     	{
     		if(i==0)
     		{
-
-   			 y[i]=y[i+1]+25;
-   			 
-   			 
+   			 y[i]=y[i+1]+UNIT_SIZE;
     		}
     		else
     		{
@@ -191,19 +250,14 @@ public class GamePanel extends JPanel implements ActionListener {
     		}
     	}
     }
-    
-    
-    
-    
+
     public void left()
     {
     	for(int i=BodyUnitsStart;i>=0;i--)
     	{
     		if(i==0)
     		{
-
-   			 x[i]=x[i+1]-25;
-   			 
+   			 x[i]=x[i+1]-UNIT_SIZE;
     		}
     		else
     		{
@@ -212,18 +266,14 @@ public class GamePanel extends JPanel implements ActionListener {
     		}
     	}
     }
-    
-    
-    
+
     public void right()
     {
     	for(int i=BodyUnitsStart;i>=0;i--)
     	{
     		if(i==0)
     		{
-
-   			 x[i]=x[i+1]+25;
-   			 
+   			 x[i]=x[i+1]+UNIT_SIZE;
     		}
     		else
     		{
@@ -235,16 +285,29 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
-		move();
-		Eaton();
-		repaint();
-		
+		if(!collision())
+		{
+				move();
+				Eaton();
+				repaint();
+		}
+		else
+		{
+			lose();
+		}
+
+
 	}
 
-    public class MyKeyAdaptor extends KeyAdapter {
+	private void lose() {
 
-    	
+	}
+
+	public int score() {
+		return AppleEaton;
+	}
+
+	public class MyKeyAdaptor extends KeyAdapter {
 
     	@Override
     	public void keyPressed(KeyEvent e) {
@@ -252,21 +315,34 @@ public class GamePanel extends JPanel implements ActionListener {
     		System.out.println("pressed");
     		switch(e.getKeyCode())
     		{
-    		case KeyEvent.VK_W:
-    			keyPressed='w';
+    		case KeyEvent.VK_W:case KeyEvent.VK_UP:
+			{
+				if(keyPressed!='s') {
+
+					keyPressed='w';
+				}
+				break;
+			}
+
+    		case KeyEvent.VK_A:case KeyEvent.VK_LEFT:
+				if(keyPressed!='d')
+				{
+				keyPressed = 'a';
+				}
     			break;
-    		case KeyEvent.VK_A:
-    			keyPressed='a';
+    		case KeyEvent.VK_S:case KeyEvent.VK_DOWN:
+				if(keyPressed!='w')
+				{
+				keyPressed = 's';
+				}
     			break;
-    		case KeyEvent.VK_S:
-    			keyPressed='s';
-    			break;
-    		case KeyEvent.VK_D:
-    			keyPressed='d';
-    			break;
-    		default:
-    			keyPressed='s';
-    			break;
+    		case KeyEvent.VK_D:case KeyEvent.VK_RIGHT:
+				if(keyPressed!='a')
+				{
+					keyPressed = 'd';
+				}
+				break;
+
     		}
     	}
 }
